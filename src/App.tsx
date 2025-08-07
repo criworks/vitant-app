accff1d3775dd21c228b5ebe969565bbe25d4471
criworks <cri@cri.works>
2025-08-07 13:57:55 -0400

    Refactor: update layout structure, remove unused CSS and set theme files

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,37 @@
 import { Routes, Route } from 'react-router-dom';
 import AppLayout from './components/AppLayout';
 import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
+import SearchPage from './pages/SearchPage';
 import DesignSystemPage from './pages/DesignSystemPage';
 import ComponentPage from './pages/design-system/ComponentPage';
 import ButtonPage from './pages/design-system/components/general/ButtonPage';
-import { ConfigProvider } from 'antd';
-import themeConfig from './theme/themeConfig.tsx';
-import './index.css';
+import TestPage from './pages/TestPage';
+import AntThemeProvider from './AntThemeProvider';
+import SettingsPage from './pages/SettingsPage';
+import ReportsPage from './pages/ReportsPage';
+import ManagePage from './pages/ManagePage';
+import CalendarPage from './pages/CalendarPage';
 
 const App = () => {
   return (
-    <ConfigProvider theme={themeConfig}>
+    <AntThemeProvider>
       <Routes>
         <Route path="/" element={<AppLayout />}>
           <Route index element={<HomePage />} />
-          <Route path="page" element={<AboutPage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="manage" element={<ManagePage />} />
+          <Route path="settings" element={<SettingsPage />} />
+          <Route path="calendar" element={<CalendarPage />} />
+          <Route path="reports" element={<ReportsPage />} />
+          <Route path="tests" element={<TestPage />} />
           <Route path="design-system" element={<DesignSystemPage />} />
           <Route path="design-system/:category" element={<ComponentPage />} />
           <Route path="design-system/:category/:subcategory" element={<ComponentPage />} />
           <Route path="design-system/components/general/button" element={<ButtonPage />} />
           <Route path="design-system/:category/:subcategory/:component" element={<ComponentPage />} />
-        </Route>
+          </Route>
       </Routes>
-    </ConfigProvider>
+    </AntThemeProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-// import './index.css' // Descomentar o añadir esta línea
 import 'antd/dist/reset.css'; // Resetear los estilos de browser
 import { BrowserRouter } from 'react-router-dom'; // Envuelve App con BrowserRouter
 
